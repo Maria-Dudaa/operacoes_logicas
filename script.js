71f015d8da09e636401e5ff07a2aeca32dbe414d
Duda <mariaedavlis.s@gmail.com>
2024-11-16 10:11:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let numero = prompt("Digite um número:");
+
+// Converte o valor digitado para número
+numero = Number(numero);
+
+if (numero > 10) {
+    alert("O número é maior ou igual a 10.");
+} else {
+    alert("O número é menor que 10.");
+}
